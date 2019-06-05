db7e628f0a84d9a8c2e2decb4f112acc6724626b
thulani196 <tembothulani96@outlook.com>
2019-06-05 12:37:37 +0200

    Added optional name parameter to getAllProvinces()

--- lib/controllers/province_controller.dart
+++ lib/controllers/province_controller.dart
@@ -8,14 +8,19 @@ class ProvinceController extends ResourceController {
   ProvinceController(this.context);
   final ManagedContext context;
 
+  // Get Function that receives name as an optional parameter and get all provinces matching the name if it is parsed
+  // Otherwise, it gets all provinces in the model.
   @Operation.get()
-  Future<Response> getAllProvinces() async {
+  Future<Response> getAllProvinces({@Bind.query('name') String name}) async {
 
     final query = Query<Province>(context);
+
+    if(name != null) {
+      query.where((province) => province.name).contains(name, caseSensitive: false);
+    }
+
     final res = await query.fetch();
-
     return Response.ok(res);
-
   }
 
 }
